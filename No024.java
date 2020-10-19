3d86c27524747d4d9410cb8200838cb34f80159c
LosLiSang <52745915+LosLiSang@users.noreply.github.com>
2020-10-19 20:06:56 +0800

    master

--- No024.java
+++ No024.java
@@ -13,12 +13,27 @@ public class No024 {
     
     static class Solution {
         public ListNode swapPairs(ListNode head) {
-            ListNode fakeHead = new ListNode(0, head);
-            head = fakeHead;
+            ListNode itr = new ListNode(0, head);
+            head = itr;
+            while(itr != null && itr.next != null && itr.next.next != null){
+                swap(itr, itr.next, itr.next.next);
+                itr = itr.next.next;
+            }
             return head.next;
         }
-        private void swap(){
-            
+        private void swap(ListNode n1f, ListNode n1, ListNode n2f, ListNode n2){
+            ListNode n1r = n1.next, n2r = n2.next;
+            n1f.next = n2;
+            n2f.next = n1;
+            n1.next = n2r;
+            n2.next = n1r;
+        }
+
+        private void swap(ListNode n1f, ListNode n1, ListNode n2){
+            ListNode n2r = n2.next;
+            n1f.next = n2;
+            n1.next = n2r;
+            n2.next = n1;
         }
     }
 }
